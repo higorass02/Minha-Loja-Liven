f1be31703e3a91d4b64f61139dea329c4344da38
Higor Santos <higoraugusto120@gmail.com>
2021-11-16 18:41:28 -0300

    iniciando projeto

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Index from './src/index'
+
+const App = () => {
+    return(
+        <Index/>
+    )
+}
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import style from './index_style'
+
+const Index = () => {
+    return(
+        <View style={style.textContent}>
+            <Text>Hello Liven!</Text>
+        </View>
+    )
+}
+export default Index
